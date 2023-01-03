7930ffb3ec56a3c65d233ddaa8391376fbe69e76
DataScienceMLGrpM <121807393+DataScienceMLGrpM@users.noreply.github.com>
2023-01-03 04:22:00 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@
 import flask
 import pandas as pd
 
-from model.string_matcher import StringMatcher
+from model.credit_card_default_prediction import CreditCardDefaultPrediction
 from settings import config
 
 ## app
@@ -24,28 +24,24 @@ def index():
 	try:
 		if flask.request.method == 'POST':
 			## data from client
+			## data from client
 			app.logger.info(flask.request.files)
 			app.logger.info(flask.request.form)
-			dtf_lookup = pd.read_excel(flask.request.files["dtf_lookup"])
-			dtf_match = pd.read_excel(flask.request.files["dtf_match"])
-			threshold = float(flask.request.form["threshold"])
-			top = 1 if flask.request.form["top"].strip() == "" else int(flask.request.form["top"])
+			data = pd.read_excel(flask.request.files["data"])
+			model_name = flask.request.form["model"]
 			app.logger.warning("--- Inputs Received ---")
-
-			## match
-			model = StringMatcher(dtf_lookup, dtf_match)
-			dtf_out = model.vlookup(threshold=threshold, top=top)
-			xlsx_out = model.write_excel(dtf_out)
-			return flask.send_file(xlsx_out, attachment_filename='StringsMatcher.xlsx', as_attachment=True)             
+					## predict
+			model = CreditCardDefaultPrediction(data)
+			predictions = model.predict(model_name)
+			xlsx_out = model.write_excel(predictions)
+			return flask.send_file(xlsx_out, attachment_filename='CreditCardDefaultPrediction.xlsx', as_attachment=True)             
 		else:
 			return flask.render_template("index.html")
 
 	except Exception as e:
 		app.logger.error(e)
 		flask.abort(500)
-    
-
-   
+ 
 # errors
 @app.errorhandler(404)
 def page_not_found(e):
@@ -55,7 +51,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return flask.render_template('errors.html', msg="Something went terribly wrong"), 500
-
 
 
 # run
