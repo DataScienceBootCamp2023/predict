214134ca4eceb9ce6fbe9bf23c758d903d948e71
DataScienceMLGrpM <121807393+DataScienceMLGrpM@users.noreply.github.com>
2023-01-06 04:17:35 +0300

    Update credit_card_default_prediction.py

--- model/credit_card_default_prediction.py
+++ model/credit_card_default_prediction.py
@@ -1,33 +1,26 @@
 import pandas as pd
-import numpy as np
-from sklearn import feature_extraction, metrics
 import io
-from tqdm import tqdm
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
 
-class CreditCardDefaultPrediction():
-    def __init__(self, dtf_input):
-      self.dtf_input = dtf_input
+class CreditCardDefaultPrediction:
+    def __init__(self, model_file=None):
+        if model_file:
+            self.model = joblib.load(model_file)
+        else:
+            self.model = LogisticRegression()
 
-
-    @staticmethod
-    def predict(dtf_input):
-        ## fit model
-        model = RandomForestClassifier()
-        model.fit(dtf_input.iloc[:, :-1], dtf_input.iloc[:, -1])
-
-        ## make predictions
-        predictions = model.predict(dtf_input.iloc[:, :-1])
-        dtf_predictions = pd.DataFrame(predictions, columns=["default"])
-        dtf_predictions["default"] = dtf_predictions["default"].apply(lambda x: "Yes" if x==1 else "No")
-
-        return dtf_predictions
-
-
-    @staticmethod
-    def write_excel(dtf_predictions):
-        bytes_file = io.BytesIO()
-        excel_writer = pd.ExcelWriter(bytes_file)
-        dtf_predictions.to_excel(excel_writer, sheet_name='Sheet1', na_rep='', index=False)
-        excel_writer.save()
-        bytes_file.seek(0)
-        return bytes_file
+    def predict(self, input_data):
+        # Make predictions using the input data
+        predictions = self.model.predict(input_data)
+        
+        # Add the predictions to the input data as a new column
+        input_data['default'] = predictions
+        
+        # Return the input data with the predictions
+        return input_data
+    
+    def save_model(self, model_file):
+        # Save the model to the specified file
+        joblib.dump(self.model, model_file)
