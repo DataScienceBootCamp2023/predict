ce1099fa73f6ea63cfe938f69c3364d72190a04d
DataScienceMLGrpM <121807393+DataScienceMLGrpM@users.noreply.github.com>
2023-01-07 23:03:01 +0300

    Update credit_card_default_prediction.py

--- model/credit_card_default_prediction.py
+++ model/credit_card_default_prediction.py
@@ -51,7 +51,7 @@ class CreditCardDefaultPrediction():
         predictions = model.predict(self.dtf_input.iloc[:, 1:-1])
         # Create a dataframe with the predictions and the original data
         dtf_predictions = pd.concat([self.dtf_input.iloc[:, 0], pd.DataFrame(predictions, columns=["default"])], axis=1)
-        dtf_predictions["default"] = dtf_predictions["default"].apply(lambda x: "Yes" if x==1 else "No"
+        dtf_predictions["default"] = dtf_predictions["default"].apply(lambda x: "Yes" if x==1 else "No")
 
         return dtf_predictions
 
